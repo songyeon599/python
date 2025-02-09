a4aaf49e88de3cc0dfdd468d2920c3852692ca04
songyeon599 <imsongyeon599@gmail.com>
2025-02-09 15:34:15 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
 print('hello word')
+
+name = "임송연"
+age = 17
+print('hello!, my name is', name, 'my age is', age, '.' )
+
+num1 = 5
+num2 = 3
+num3 = num1+num2
+print(num1+num2, )
+
+color = "blue"
+print('my favorite color is', color, '!')
+
+string = input("당신의 이름을 입력하세요:")
+print("안녕하세요", string, "님")
+
